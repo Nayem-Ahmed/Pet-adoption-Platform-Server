4000bdf77f14e2ce5559bffa8d4d4433140a10dd
unknown <freelancernayem.me@gmail.comm>
2024-02-04 11:39:21 +0600

    save user

--- index.js
+++ index.js
@@ -24,17 +24,57 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         const listingCollection = client.db('PetAdoption').collection('petListion')
+        const usersCollection = client.db('PetAdoption').collection('users')
+        const adoptCollection = client.db('PetAdoption').collection('adoption')
 
-
+        // Get  all petlisting
         app.get('/petlisting', async (req, res) => {
             const result = await listingCollection.find().toArray()
             res.send(result)
         })
 
-        // Get single products
+        // Get single petlisting
         app.get('/petlisting/:id', async (req, res) => {
             const id = req.params.id
-            const result = await  listingCollection.findOne({ _id: new ObjectId(id) })
+            const result = await listingCollection.findOne({ _id: new ObjectId(id) })
+            res.send(result)
+        })
+
+        // Save or modify user email, status in DB
+        app.put('/users/:email', async (req, res) => {
+            const email = req.params.email
+            const user = req.body
+            const query = { email: email }
+            const options = { upsert: true }
+            const isExist = await usersCollection.findOne(query)
+            console.log('User found?----->', isExist)
+            if (isExist) {
+                if (user?.status === 'Requested') {
+                    const result = await usersCollection.updateOne(
+                        query,
+                        {
+                            $set: user,
+                        },
+                        options
+                    )
+                    return res.send(result)
+                } else {
+                    return res.send(isExist)
+                }
+            }
+            const result = await usersCollection.updateOne(
+                query,
+                {
+                    $set: { ...user, timestamp: Date.now() },
+                },
+                options
+            )
+            res.send(result)
+        })
+        // adoption
+        app.post('/adoption', async (req, res) => {
+            const adopt = req.body;
+            const result = await adoptCollection.insertOne(adopt)
             res.send(result)
         })
 
